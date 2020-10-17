d6f8f32299cdc47712e19eb3f80ae8e143bc8ec4
Charles DePalma <Charles DePalma>
2020-10-17 18:15:40 -0400

    fix types in header

--- materials/roundaboutheader.h
+++ materials/roundaboutheader.h
@@ -6,16 +6,16 @@ typedef uint8_t  rb_angle8;
 typedef uint16_t rb_angle16;
 typedef uint32_t rb_angle32;
 
-typedef rb_angle8  rb_q0_8;
-typedef rb_angle16 rb_q0_16;
-typedef rb_angle32 rb_q0_32;
+typedef rb_angle8  rb_uq0_8;
+typedef rb_angle16 rb_uq0_16;
+typedef rb_angle32 rb_uq0_32;
 
 typedef int8_t  rb_q1_7;
 typedef int16_t rb_q1_15;
 typedef int32_t rb_q1_31;
 
 // This is a LUT routine, but it demonstrates coalescion pretty well.
-rb_sfixed8q7 rb_sin8_qlut7( rb_angle8 x, uint8_t * lut )
+rb_q1_7 rb_sin8_lut( rb_angle8 x, rb_q1_7 * lut )
 {
 	_Bool negative = 0;
 	if ( x > 0x7F )
